3485e18b7c68c285f032cb77319d4952c8ab978b
gemamm <gemamm1988@gmail.com>
2021-10-15 10:55:29 +0200

    act

--- src/com/ceep/test/Test.java
+++ src/com/ceep/test/Test.java
@@ -3,6 +3,7 @@ package com.ceep.test;
 
 import accesoDatos.IaccesoDatos;
 import accesoDatos.ImplementacionMysql;
+import accesoDatos.ImplementacionOracle;
 
 
 public class Test {
@@ -10,7 +11,9 @@ public class Test {
     public static void main(String[] args) {
         
         IaccesoDatos  datos= new ImplementacionMysql();
+        IaccesoDatos  datos1= new ImplementacionOracle();
         datos.listar();
+        datos1.insertar();
         
         
     }
